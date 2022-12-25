e7fa4e2929e200d596c2222be023ae4f3e02e0ee
Mathewos Wondwosen <89460058+mathew2005@users.noreply.github.com>
2022-12-25 10:00:25 +0300

    glow effect, scroll down text

--- js/script.js
+++ js/script.js
@@ -30,3 +30,25 @@ var typed = new Typed(".auto-input",{
 //time for copyright
 let copyrightTime = document.querySelector('time');
 copyrightTime.textContent = new Date().getFullYear();
+
+
+// back to top button with progress bar
+let calcScrollValue = () =>{
+	let scrollProgress = document.getElementById("progress");
+	let progressValue = document.getElementById('progress-value');
+	let pos = document.documentElement.scrollTop;
+	let calcHeight  = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	let scrollValue = Math.round((pos * 100) / calcHeight);
+	if(pos > 100){
+		scrollProgress.style.display = 'grid';
+	}
+	else{
+		scrollProgress.style.display = 'none';
+	}
+scrollProgress.addEventListener('click', function(){
+	document.documentElement.scrollTop = 0;
+});
+scrollProgress.style.background = `conic-gradient(var(--main-color) ${scrollValue}%, #d7d7d7 ${scrollValue + 2}%)`
+}
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
